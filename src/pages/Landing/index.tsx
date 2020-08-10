97f945310a306b95efc1e684796ee1cf5324fadf
Wesley Ferreira <wesley.ferreira@finplace.com.br>
2020-08-10 18:52:12 -0300

    landing page css

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -27,12 +27,12 @@ function Landing() {
             />
 
             <div className="buttons-container">
-                <a href="" className="study">
-                    <img src={studyIcon} alt="Estudar"/>
+                <a href="" className="study"> 
+                    <img src={studyIcon} alt="Estudar"/>Estudar
                 </a>
 
                 <a href="" className="give-classes">
-                    <img src={giveClassesIcon} alt="Dar Aulas"/>
+                    <img src={giveClassesIcon} alt="Dar Aulas"/>Dar Aulas
                 </a>
             </div>
              <span className="total-connections">
